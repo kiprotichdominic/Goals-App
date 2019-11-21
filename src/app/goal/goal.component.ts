56b3cf5757e1547e0db88df541c019ea7f72e221
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-11-21 15:17:54 +0300

    final push

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -19,7 +19,7 @@ export class GoalComponent implements OnInit {
       1,
       "Watch finding Nemo",
       "Find an online version and watch merlin find his son",
-      new Date(2020, 3, 14)
+      new Date(2019, 9, 14)
     ),
     new Goal(
       2,
@@ -31,33 +31,41 @@ export class GoalComponent implements OnInit {
       3,
       "Get new Phone Case",
       "Diana has her birthday coming up soon",
-      new Date(2022, 1, 12)
+      new Date(2019, 1, 12)
     ),
     new Goal(
       4,
       "Get Dog Food",
       "Pupper likes expensive snacks",
-      new Date(2019, 0, 18)
+      new Date(2019, 11, 18)
     ),
     new Goal(5, "Solve math homework", "Damn Math", new Date(2019, 2, 14)),
     new Goal(
       6,
       "Plot my world domination plan",
       "Cause I am an evil overlord",
-      new Date(2030, 3, 14)
+      new Date(2019, 3, 14)
     )
   ];
   toggleDetails(index) {
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
-  deleteGoal(isComplete, index){
+  deleteGoal(isComplete, index) {
     if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+      let toDelete = confirm(
+        `Are you sure you want to delete ${this.goals[index].name}?`
+      );
 
-      if (toDelete){
-        this.goals.splice(index,1)
+      if (toDelete) {
+        this.goals.splice(index, 1);
       }
     }
+  }
+  addNewGoal(goal) {
+    let goalLength = this.goals.length;
+    goal.id = goalLength + 1;
+    goal.completeDate = new Date(goal.completeDate);
+    this.goals.push(goal);
   }
 
   constructor() {}
